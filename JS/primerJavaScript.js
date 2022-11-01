05cda8d3d9ffdfbbad093ff61fb2c50621e36daf
Chaparro10 <114444946+Chaparro10@users.noreply.github.com>
2022-11-01 15:52:39 -0700

    Primer Practica JavaScript
    Primer Practica JavaScript

---
+++ JS/primerJavaScript.js
@@ -0,0 +1,14 @@
+alert("Alerta en archivo")
+
+function quitarSitec(){
+    let pregunta=confirm("seguro quiere quitar sitec");
+    if(!pregunta == false){
+        alert("SITEC borrado perrxx");
+        let si =document.getElementById("Sitec");
+        si.innerHTML ="ESTAS DEMENTE VIEJOO###";
+    }
+}
+
+function funcionAlerta(){//funciones javaScript
+    alert("di click");
+}
