19518fc858146ad1ce538ca72fd614ae22f610a9
Chaparro10 <114444946+Chaparro10@users.noreply.github.com>
2022-11-03 15:44:25 -0700

    JavaScript api
    api 1ra parte

--- JS/primerJavaScript.js
+++ JS/primerJavaScript.js
@@ -1,5 +1,14 @@
 alert("Alerta en archivo")
 
+
+async function traePersona(){
+    const respuesta=await fetch("https://randomuser.me/api/");
+    const datos=await respuesta.json();
+    console.log(datos.results[0].name.first+" "+datos.results[0].name.last);
+    
+}
+
+
 function quitarSitec(){
     let pregunta=confirm("seguro quiere quitar sitec");
     if(!pregunta == false){
